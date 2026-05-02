d39cfd3a89dc9e9e9c8027887e9270acae44d3a6
pminhas24 <parneet2411@gmail.com>
2026-05-01 18:23:55 -0700

    functional Lisp interpreter

--- CMPS3500_MiniScheme_Group07/functional/minischeme_reader.lisp
+++ CMPS3500_MiniScheme_Group07/functional/minischeme_reader.lisp
@@ -1,4 +1,5 @@
 ;;; minischeme_reader.lisp
+;;; Checkpoint 3 Functional MiniScheme Interpreter
 
 (defun read-file-as-string (path)
   (with-open-file (in path :direction :input)
@@ -57,8 +58,10 @@
     (cond
       ((string= token "(")
        (parse-list (cdr tokens) '()))
+
       ((string= token ")")
        (error "PARSE_ERROR"))
+
       (t
        (values (atom-from-token token) (cdr tokens))))))
 
@@ -70,28 +73,55 @@
     (cond
       ((string= token ")")
        (values (reverse acc) (cdr tokens)))
+
       (t
        (multiple-value-bind (expr rest) (parse-expression tokens)
          (parse-list rest (cons expr acc)))))))
 
-(defun parse-tokens (tokens)
-  (multiple-value-bind (expr rest) (parse-expression tokens)
-    (if (null rest)
-        expr
-        (error "PARSE_ERROR"))))
+(defun parse-program (tokens)
+  (let ((exprs '()))
+    (loop while tokens do
+      (multiple-value-bind (expr rest) (parse-expression tokens)
+        (push expr exprs)
+        (setf tokens rest)))
+    (reverse exprs)))
 
 ;;;; =========================
-;;;; Evaluator
+;;;; Environment
 ;;;; =========================
 
-(defun env-lookup (name env)
+(defun make-cell (value)
+  (cons value nil))
+
+(defun cell-value (cell)
+  (car cell))
+
+(defun set-cell-value (cell value)
+  (setf (car cell) value))
+
+(defun env-lookup-cell (name env)
   (let ((binding (assoc name env)))
     (if binding
         (cdr binding)
         (error "UNDECLARED_IDENTIFIER"))))
 
+(defun env-lookup (name env)
+  (cell-value (env-lookup-cell name env)))
+
 (defun env-extend (vars vals env)
-  (append (pairlis vars vals) env))
+  (append
+   (mapcar (lambda (var val)
+             (cons var (make-cell val)))
+           vars
+           vals)
+   env))
+
+(defun env-extend-with-cells (vars cells env)
+  (append (mapcar #'cons vars cells) env))
+
+;;;; =========================
+;;;; Values / Closures
+;;;; =========================
 
 (defun bool-value-p (x)
   (or (eq x t) (eq x nil)))
@@ -104,6 +134,15 @@
        (>= (length value) 4)
        (eq (first value) 'CLOSURE)))
 
+(defun closure-params (closure)
+  (second closure))
+
+(defun closure-body-forms (closure)
+  (third closure))
+
+(defun closure-env (closure)
+  (fourth closure))
+
 (defun type-name (value)
   (cond
     ((integerp value) "int")
@@ -111,13 +150,31 @@
     ((closure-p value) "function")
     (t "unknown")))
 
-(defun eval-args (args env)
-  (mapcar (lambda (arg) (evaluate arg env)) args))
+(defun print-value (value)
+  (cond
+    ((eq value t) "#t")
+    ((eq value nil) "#f")
+    ((closure-p value) "<function>")
+    (t value)))
+
+;;;; =========================
+;;;; Evaluator Helpers
+;;;; =========================
 
 (defun ensure-integer (value)
   (if (integerp value)
       value
       (error "TYPE_MISMATCH")))
+
+(defun eval-args (args env)
+  (mapcar (lambda (arg) (evaluate arg env)) args))
+
+(defun eval-sequence (forms env)
+  (if (null forms)
+      (error "PARSE_ERROR")
+      (let ((result nil))
+        (dolist (form forms result)
+          (setf result (evaluate form env))))))
 
 (defun eval-arithmetic (op args env)
   (if (/= (length args) 2)
@@ -148,27 +205,11 @@
 
 (defun eval-if (args env)
   (if (/= (length args) 3)
-      (error "WRONG_ARITY")
+      (error "PARSE_ERROR")
       (let ((condition (evaluate (first args) env)))
         (if condition
             (evaluate (second args) env)
             (evaluate (third args) env)))))
-
-(defun closure-params (closure)
-  (second closure))
-
-(defun closure-body-forms (closure)
-  (third closure))
-
-(defun closure-env (closure)
-  (fourth closure))
-
-(defun eval-sequence (forms env)
-  (if (null forms)
-      (error "PARSE_ERROR")
-      (let ((result nil))
-        (dolist (form forms result)
-          (setf result (evaluate form env))))))
 
 (defun eval-let (args env)
   (if (< (length args) 2)
@@ -177,6 +218,7 @@
              (body-forms (rest args)))
         (unless (listp bindings)
           (error "PARSE_ERROR"))
+
         (let ((vars '())
               (vals '()))
           (dolist (binding bindings)
@@ -184,10 +226,14 @@
                          (= (length binding) 2)
                          (symbolp (first binding)))
               (error "PARSE_ERROR"))
+
             (push (first binding) vars)
             (push (evaluate (second binding) env) vals))
+
           (eval-sequence body-forms
-                         (env-extend (reverse vars) (reverse vals) env))))))
+                         (env-extend (reverse vars)
+                                     (reverse vals)
+                                     env))))))
 
 (defun eval-lambda (args env)
   (if (< (length args) 2)
@@ -196,9 +242,11 @@
             (body-forms (rest args)))
         (unless (listp params)
           (error "PARSE_ERROR"))
+
         (dolist (p params)
           (unless (symbolp p)
             (error "PARSE_ERROR")))
+
         (make-closure params body-forms env))))
 
 (defun apply-function (fn args env)
@@ -208,17 +256,58 @@
        (let ((params (closure-params fn))
              (body-forms (closure-body-forms fn))
              (saved-env (closure-env fn)))
+
          (if (/= (length params) (length arg-values))
              (error "WRONG_ARITY")
              (eval-sequence body-forms
                             (env-extend params arg-values saved-env)))))
+
       (t
        (error "TYPE_MISMATCH")))))
+
+(defun eval-define (args env)
+  (unless (= (length args) 2)
+    (error "PARSE_ERROR"))
+
+  (let ((name (first args))
+        (value-expr (second args)))
+    (unless (symbolp name)
+      (error "PARSE_ERROR"))
+
+    ;; Placeholder cell allows recursive functions to reference themselves.
+    (let* ((cell (make-cell nil))
+           (new-env (env-extend-with-cells (list name) (list cell) env))
+           (value (evaluate value-expr new-env)))
+
+      (set-cell-value cell value)
+
+      ;; Mutate global environment list by returning name/value pair through special handling.
+      (values name cell))))
+
+(defun eval-cond (args env)
+  (dolist (clause args)
+    (unless (and (listp clause)
+                 (= (length clause) 2))
+      (error "PARSE_ERROR"))
+
+    (let ((test (first clause))
+          (body (second clause)))
+      (when (or (eq test 'ELSE)
+                (evaluate test env))
+        (return-from eval-cond (evaluate body env)))))
+
+  (error "EVAL_ERROR"))
+
+;;;; =========================
+;;;; Main Evaluator
+;;;; =========================
 
 (defun evaluate (expr env)
   (cond
     ((integerp expr) expr)
+
     ((eq expr t) t)
+
     ((eq expr nil) nil)
 
     ((symbolp expr)
@@ -243,24 +332,57 @@
          ((eq op 'LAMBDA)
           (eval-lambda args env))
 
+         ((eq op 'COND)
+          (eval-cond args env))
+
+         ((eq op 'DEFINE)
+          (error "DEFINE_ONLY_ALLOWED_AT_TOP_LEVEL"))
+
          (t
           (apply-function (evaluate op env) args env)))))
 
     (t
      (error "EVAL_ERROR"))))
 
-(defun print-result (value)
-  (format t "Status: OK~%")
-  (cond
-    ((eq value t)
-     (format t "Result: #t~%"))
-    ((eq value nil)
-     (format t "Result: #f~%"))
-    ((closure-p value)
-     (format t "Result: <function>~%"))
-    (t
-     (format t "Result: ~a~%" value)))
-  (format t "Type: ~a~%" (type-name value)))
+(defun eval-program (exprs)
+  (let ((env '())
+        (result nil))
+    (dolist (expr exprs result)
+      (if (and (listp expr) (eq (first expr) 'DEFINE))
+          (multiple-value-bind (name cell) (eval-define (rest expr) env)
+            (setf env (env-extend-with-cells (list name) (list cell) env))
+            (setf result name))
+          (setf result (evaluate expr env))))))
+
+;;;; =========================
+;;;; Output
+;;;; =========================
+
+(defun print-result (case-path value)
+  (let ((filename (file-namestring case-path)))
+    (if (bool-value-p value)
+        (format t "~a -> Result: ~a ; Type: ~a~%"
+                filename
+                (print-value value)
+                (type-name value))
+        (format t "~a -> Status: OK | Result: ~a | Type: ~a~%"
+                filename
+                (print-value value)
+                (type-name value)))))
+
+                
+(defun print-error (case-path e)
+  (let ((filename (file-namestring case-path))
+        (msg (princ-to-string e)))
+    (let ((err-type
+           (cond
+             ((search "PARSE_ERROR" msg) "PARSE_ERROR")
+             ((search "UNDECLARED_IDENTIFIER" msg) "UNDECLARED_IDENTIFIER")
+             ((search "WRONG_ARITY" msg) "WRONG_ARITY")
+             ((search "TYPE_MISMATCH" msg) "TYPE_MISMATCH")
+             ((search "DIVISION_BY_ZERO" msg) "DIVISION_BY_ZERO")
+             (t msg))))
+      (format t "~a -> Status: ERROR | Error: ~a~%" filename err-type))))
 
 (defun main ()
   (let ((args sb-ext:*posix-argv*))
@@ -270,15 +392,10 @@
                (input (read-file-as-string path))
                (tokens (tokenize input)))
           (handler-case
-              (let* ((parsed (parse-tokens tokens))
-                     (result (evaluate parsed '())))
-                (format t "Implementation: functional~%")
-                (format t "Case: ~a~%" path)
-                (print-result result))
+              (let* ((exprs (parse-program tokens))
+                     (result (eval-program exprs)))
+                (print-result path result))
             (error (e)
-              (format t "Implementation: functional~%")
-              (format t "Case: ~a~%" path)
-              (format t "Status: ERROR~%")
-              (format t "Error: ~a~%" e)))))))
+              (print-error path e)))))))
 
 (main)
